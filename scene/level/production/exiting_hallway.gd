de96592165d91ce659ef028aa5be33b55badf6dd
Chivorn99 <134765920+Chivorn99@users.noreply.github.com>
2026-03-12 02:16:54 +0700

    Add exit functionality between apartment and outside world with new scripts and scene updates

---
+++ scene/level/production/exiting_hallway.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var player_at_door = false
+
+# IMPORTANT: You can get the exact perfect path by going to your FileSystem,
+# right-clicking your apartment_hall_way.tscn file, and clicking "Copy Path".
+var outside_level_path = "res://scene/level/production/outside_world.tscn"  
+
+func _process(delta):
+	# If player is on the exit mat and presses 'E'
+	if player_at_door and Input.is_key_pressed(KEY_E):
+		print("Exiting shop...")
+		get_tree().change_scene_to_file(outside_level_path)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_at_door = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_at_door = false
